5230c0ae1c568e8e2cc3fbbdd67f330c3a8e478b
DeadBush03 <76849771+DeadBush03@users.noreply.github.com>
2024-10-24 11:26:58 -0500

    Update Student_Mngmnt_Group_Project.java
    added extra description

--- Student_Mngmnt_Group_Project/src/main/java/com/mycompany/student_mngmnt_group_project/Student_Mngmnt_Group_Project.java
+++ Student_Mngmnt_Group_Project/src/main/java/com/mycompany/student_mngmnt_group_project/Student_Mngmnt_Group_Project.java
@@ -1,6 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.student_mngmnt_group_project;
 
@@ -10,9 +7,26 @@ package com.mycompany.student_mngmnt_group_project;
  * Caleb Westerman, 
  * Jonathan Embler
  */
+/*
+This project is a Student Management System 
+that should be able to handle various operations,
+including adding students, tracking grades, 
+calculating averages, and generating reports (highest/lowest grades),
+and File input/output for loading/saving information respectively
+*/
+
+//scanner and file manipulation
+import java.util.Scanner;
+import java.io.*;
+
+//Please remember to put your name before the class/method you're working on
+//this is to prevent people from accidentally working on the same thing
+
 public class Student_Mngmnt_Group_Project {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         System.out.println("Hello World!");
-    }
-}
+    }// end main
+    
+}// END OF CLASS
