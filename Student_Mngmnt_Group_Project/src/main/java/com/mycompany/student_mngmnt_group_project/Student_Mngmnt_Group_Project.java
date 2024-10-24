a1517a3b6b5f8cae5b02ecca35be7ff2844e3b0a
DeadBush03 <76849771+DeadBush03@users.noreply.github.com>
2024-10-24 11:10:56 -0500

    Add files via upload

---
+++ Student_Mngmnt_Group_Project/src/main/java/com/mycompany/student_mngmnt_group_project/Student_Mngmnt_Group_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.student_mngmnt_group_project;
+
+/**
+ *
+ * @author deadb
+ */
+public class Student_Mngmnt_Group_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
