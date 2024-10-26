ae71ac6fd6467b1ec9392b6f77a55ee6a146483f
DeadBush03 <76849771+DeadBush03@users.noreply.github.com>
2024-10-26 18:54:34 -0500

    Update Student_Mngmnt_Group_Project.java

--- Student_Mngmnt_Group_Project/src/main/java/com/mycompany/student_mngmnt_group_project/Student_Mngmnt_Group_Project.java
+++ Student_Mngmnt_Group_Project/src/main/java/com/mycompany/student_mngmnt_group_project/Student_Mngmnt_Group_Project.java
@@ -1,18 +1,41 @@
 
 package com.mycompany.student_mngmnt_group_project;
 
-/**
- *
+/*
  * @authors
  * Caleb Westerman, 
  * Jonathan Embler
+ * Jack
  */
+
+//PROJECT REQUIREMENTS
+
 /*
+-----
 This project is a Student Management System 
 that should be able to handle various operations,
 including adding students, tracking grades, 
 calculating averages, and generating reports (highest/lowest grades),
 and File input/output for loading/saving information respectively
+-----
+The system should display a menu with options for users to select operations 
+like adding, removing, or viewing student details
+-----
+At LEAST three key classes:
+- `Student` (attributes: ID, name, grades, etc.)
+- `Subject` (attributes: name, grade, etc.)
+- `StudentManagementSystem` (handling all system operations)
+
+- Apply the principles of Encapsulation to ensure data hiding.
+- Use Inheritance and Polymorphism where appropriate
+
+REPORTS:
+The system should generate reports such as
+- Average grade for each student
+- Highest and lowest grades for a specific subject
+- A list of students sorted by grade
+
+Remember to ensure error handling and input validation
 */
 
 //scanner and file manipulation
