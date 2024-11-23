a32cd7a26c1b579160d1491cd332c034a452e40d
DeadBush03 <76849771+DeadBush03@users.noreply.github.com>
2024-11-23 17:56:25 -0600

    Update Student_Mngmnt_Group_Project.java
    Made some horrible code that completes case 1's functions.
    Made a terribly inefficient and bad search algorithm for case 3's function.
    
    Attempted but failed to make case 3 function with horrible code.
    Do not hesitate to replace anything with a better method.

--- Student_Mngmnt_Group_Project/src/main/java/com/mycompany/student_mngmnt_group_project/Student_Mngmnt_Group_Project.java
+++ Student_Mngmnt_Group_Project/src/main/java/com/mycompany/student_mngmnt_group_project/Student_Mngmnt_Group_Project.java
@@ -99,37 +99,99 @@ public class StudentManagementSystem
                     
                 case 1: //student information
                     System.out.println("You chose to Alter Student Information");
-                    //call student manual input
+                    //ask user if they want to add/remove a student
+                    System.out.println("Would you like to add or remove a student?"
+                            + "\n-1 - Exit\n0 - add student\n1 - remove student");
+                    int AddRemove = 2; //nonsense starting value
                     
-                    //Probably ask user if they want to add/remove a student (with validation in case the user tries anything impossible)
-                    //add
-                    System.out.println("What subject do you want to add one student to?");
-                    for (int x = 0; x < ClassSubjects.length; x++)
-                    {//present the possible choices of subject
-                        System.out.println("Enter " + x + ": " + ClassSubjects[x].getName() );
-                    }
-			System.out.println("Or Enter -1 to exit");
-                    //get the index directly from the user
-                    int userInputSubject = keyboard.nextInt();
-                    //validation
-                    while (userInputSubject < -1 || userInputSubject > ClassSubjects.length)
-                        {//if user input is invalid
-                        System.out.println("Your input was invalid, re-enter your choice: ");
-                        userInputSubject = keyboard.nextInt();
-                        } //keep re-taking input until they are within a valid range
+                    //validation//
+                    AddRemove = keyboard.nextInt();
+                    do //repeat what the user said until they choose to exit
+                    {
+                    //if 1 or 0, do something now that we've validated
+                    if (AddRemove == 0)
+                    {
+                        //add
+                        System.out.println("What subject do you want to add one student to?");
+                        for (int x = 0; x < ClassSubjects.length; x++)
+                        {//present the possible choices of subject
+                            System.out.println("Enter " + x + ": " + ClassSubjects[x].getName() );
+                        }
+                            System.out.println("Or Enter -1 to exit");
+                        //get the index directly from the user
+                        int userInputSubject = keyboard.nextInt();
+                        //validation
+                        while (userInputSubject < -1 || userInputSubject > ClassSubjects.length)
+                            {//if user input is invalid
+                            System.out.println("Your input was invalid, re-enter your choice: ");
+                            userInputSubject = keyboard.nextInt();
+                            } //keep re-taking input until they are within a valid range
 
-		    if (userInputSubject == -1) //exit only if user says to
+                        if (userInputSubject == -1) //exit only if user says to
+                        {break;}
+                        
+                        System.out.println("For the Subject: " + ClassSubjects[userInputSubject].getName());
+                        ClassSubjects[userInputSubject].addStudent(createStudent());
+                    }
+                    else //only other option is to remove
+                    {
+                        //get subject from user
+                            System.out.println("What subject do you want to remove a student from?");
+                            for (int x = 0; x < ClassSubjects.length; x++)
+                            {//present the possible choices of subject
+                                System.out.println("Enter " + x + ": " + ClassSubjects[x].getName() );
+                            }
+                            System.out.println("Or Enter -1 to exit");
+                            //get the index directly from the user
+                            int userInputSubject = keyboard.nextInt();
+                            //validation
+                            while (userInputSubject < -1 || userInputSubject > ClassSubjects.length)
+                            {//if user input is invalid
+                                System.out.println("Your input was invalid, re-enter your choice: ");
+                                userInputSubject = keyboard.nextInt();
+                            } //keep re-taking input until they are within a valid range
+
+                            if (userInputSubject == -1) //exit only if user says to
+                            {break;}    
+                            //then check if there are no students in the subject
+                            else if (ClassSubjects[userInputSubject].getStudents().isEmpty())
+                            {//tell the user and break if it's empty
+                                System.out.println("\nThe Subject: " + ClassSubjects[userInputSubject].getName() + " is empty, and has no students.\n");
+                                break;
+                            }
+                        System.out.println("From the subject: " + ClassSubjects[userInputSubject].getName() + 
+                                "\nEnter the ID of the student you would like to remove: ");
+                        Scanner keyboard2 = new Scanner(System.in); //new keyboard for a string
+                        String RemoveStudent = keyboard2.nextLine();
+                        
+                        for (int j = 0; j < ClassSubjects[userInputSubject].getStudents().size(); j++)
+                        {//go through every class
+                            //walk through every student
+                            if (ClassSubjects[userInputSubject].getStudents().get(j).getID().equals(RemoveStudent)) //string comparison
+                            {//if found, remove every student in the subject with that ID
+                            ClassSubjects[userInputSubject].getStudents().remove(j);
+                            }
+                        }//end search through students in a class   
+                    }//end remove student
+                    
+                    //otherwise, if user exits
+                    if (AddRemove == -1)//exit if -1
                     {break;}
-                    
-                    System.out.println("For the Subject: " + ClassSubjects[userInputSubject].getName());
-                    ClassSubjects[userInputSubject].addStudent(createStudent());
-                    
+                    else if (AddRemove != 0 && AddRemove != 1) //if it's not 0 and not 1
+                        { 
+                        System.out.println("INVALID INPUT. Please re-enter a valid number:"); 
+                        AddRemove = keyboard.nextInt(); //re-take input if invalid
+                        }
+                    } 
+                    while (AddRemove != -1); //keep repeating until user chooses to exit
+
                     break;
                     
                 case 2: //outputting to a file or importing from a file
 			    
 		    try
 		    {
+                        //NEEDS INPUT VALIDATION
 	                    System.out.println("You have chosen to Save to or Import from a Text File.");
 	                    //Asking user if they want to back out of this option, save to a file, or import from a file
 			    System.out.println("Please enter the number of the option you want to choose.");
@@ -166,7 +228,28 @@ public class StudentManagementSystem
 	                        //POTENTIAL: maybe look into having the user name the file they want to create and save to? - Jack
 	                        fileName = "StudentInfo.txt";
 	                        System.out.println("You have chosen to save your student information to file, 'StudentInfo.txt'.");
-	                        saveInfo(fileName);
+                                
+                                
+// -caleb
+/*
+I moved this to see if it would work...
+I'm not seeing a new file get made and I don't know why...
+        */
+	                        PrintWriter outputFile = new PrintWriter(fileName);
+                                for (int i = 0; i < ClassSubjects.length; i++)
+                                {
+                                    ClassSubjects[i].sortStudent();
+                                    for (int j = 0; j < ClassSubjects[i].getStudents().size(); j++)
+                                    {
+                                        outputFile.println(ClassSubjects[i].getName());
+                                        outputFile.println(ClassSubjects[i].getStudents().get(j).getName());
+                                        outputFile.println(ClassSubjects[i].getStudents().get(j).getID());
+                                        outputFile.println(ClassSubjects[i].getStudents().get(j).getGrade());
+                                        outputFile.println();
+                                    }
+                                }
+                                outputFile.close();
+                                
 	                    }
                     }//catch for if file is not found for either save option or import option
                     catch(FileNotFoundException e)
@@ -181,9 +264,46 @@ public class StudentManagementSystem
                     System.out.println("OUTPUTTING AVERAGE OF A STUDENT'S GRADE...");
                     //output the average grades for each student
                     //have user choose the ID of a student to find the average grade for
+                    String SearchID;
+                    System.out.println("Please enter the ID of the student you want to average. ex. '123':");
+                    //'keyboard' from main doesn't work because it was used for an int, this is a string
+                    Scanner keyboard2 = new Scanner(System.in);
+                    SearchID = keyboard2.nextLine();
                     
-                    //search through all Subjects, and find every student with the same ID. Sum grades, and divide by number of grades.
+                    String FoundName = "No Student"; //name to call the student
+                    double totalgrade = 0;
+                    double averagegrade = 0;
+                    double numtimes = 0;
                     
+                    for (int i = 0; i < ClassSubjects.length; i++)
+                    {
+                        ClassSubjects[i].sortStudent(); //sort a class by grade, then output each student in the class
+                        //may be useful if we implement binary search...
+                        for (int j = 0; j < ClassSubjects[i].getStudents().size(); j++)
+                        {//go through every class
+                            //walk through every student
+                            if (ClassSubjects[i].getStudents().get(j).getID().equals(SearchID)) //string comparison
+                            {
+                                while (FoundName.equals("No Student")) //only get the student's name once
+                                { FoundName = ClassSubjects[i].getStudents().get(j).getName(); }
+                                //accumulate total grade of found student
+                                totalgrade += ClassSubjects[i].getStudents().get(j).getGrade();
+                                //number of times the student is found +1
+                                numtimes++;
+                            }
+                        }//end search through students in a class   
+                    }   //at the end, we've searched through every student in every class subject
+                        //if we've found the student zero times...
+                        if (numtimes == 0)
+                        {
+                            System.out.println("Sorry, we could not find a student with the ID: " + SearchID);
+                            break;
+                        } //exit if there are no students with that ID
+                        //else...
+                        averagegrade = totalgrade/numtimes;
+                        System.out.println("\nThe student: " + FoundName + ", ID: " + SearchID +
+                                "\nhas an average grade of " + averagegrade + " across all subjects.");
+                    System.out.println(); //space for formatting
                     break;
                     
                 case 4: //student report sorted by grades
@@ -199,8 +319,7 @@ public class StudentManagementSystem
                         for (int j = 0; j < ClassSubjects[i].getStudents().size(); j++)
                         {//walk through the arraylist in each subject
                             //output a report for each student
-                            reportStudent(ClassSubjects[i].getStudents().get(j),ClassSubjects[i].getName());
-                        
+                            reportStudent( ClassSubjects[i].getStudents().get(j),ClassSubjects[i].getName() );
                         }
                     }
                     System.out.println(); //space for formatting
@@ -217,7 +336,7 @@ public class StudentManagementSystem
                     }
 			System.out.println("Or Enter -1 to exit");
                     //get the index directly from the user
-                    userInputSubject = keyboard.nextInt();
+                    int userInputSubject = keyboard.nextInt();
                     //validation
                     while (userInputSubject < -1 || userInputSubject > ClassSubjects.length)
                     {//if user input is invalid
@@ -256,20 +375,20 @@ public class StudentManagementSystem
 
     //Function to save all student information to a text file.
     //THIS FUNCTION CANNOT ACCESS ClassSubjects[i], WILL HAVE TO MOVE IT TO SOMEWHERE IT CAN ACCESS THEM - Jack
-    static void saveInfo(String FileN){
-        PrintWriter outputFile = new PrintWriter(FileN);
-        for (int i = 0; i < ClassSubjects.length; i++){
-            ClassSubjects[i].sortStudent();
-            for (int j = 0; j < ClassSubjects[i].getStudents().size(); j++){
-                outputFile.println(ClassSubjects[i].getName());
-                outputFile.println(ClassSubjects[i].getStudents().get(j).getName());
-                outputFile.println(ClassSubjects[i].getStudents().get(j).getID());
-                outputFile.println(ClassSubjects[i].getStudents().get(j).getGrade());
-                outputFile.println();
-            }
-        }
-        outputFile.close();
-    }
+//    static void saveInfo(String FileN){
+//        PrintWriter outputFile = new PrintWriter(FileN);
+//        for (int i = 0; i < ClassSubjects.length; i++){
+//            ClassSubjects[i].sortStudent();
+//            for (int j = 0; j < ClassSubjects[i].getStudents().size(); j++){
+//                outputFile.println(ClassSubjects[i].getName());
+//                outputFile.println(ClassSubjects[i].getStudents().get(j).getName());
+//                outputFile.println(ClassSubjects[i].getStudents().get(j).getID());
+//                outputFile.println(ClassSubjects[i].getStudents().get(j).getGrade());
+//                outputFile.println();
+//            }
+//        }
+//        outputFile.close();
+//    }
 	
     //Caleb
     static Student createStudent()
